97038c0ebbfb57f9d2d1637053450b1fe96e8f4a
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-10-21 18:43:33 +0200

    Add CPU memory management

---
+++ 6502sharp/classes/cpu/CPU.Memory.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Reflection;
+
+namespace _6502sharp
+{
+    public partial class CPU
+    {
+        protected byte FetchNext()
+        {
+            byte ret = _machine.Memory.Get(_machine.PC.Value);
+
+            _machine.PC.Value++;
+
+            return ret;
+        }
+
+        protected byte[] FetchMultiple(int length)
+        {
+            byte[] ret = new byte[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                ret[i] = FetchNext();
+            }
+
+            return ret;
+        }
+    }
+}
