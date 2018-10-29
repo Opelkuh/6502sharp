e3ce7be504040f13153bd58671a64d0bcee745ff
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-10-29 18:06:32 +0100

    Document LEHelper

--- 6502sharp/classes/LEHelper.cs
+++ 6502sharp/classes/LEHelper.cs
@@ -1,6 +1,14 @@
 namespace _6502sharp
 {
+    /// <summary>
+    /// Little-endian coverter
+    /// </summary>
     public static class LEHelper {
+        /// <summary>
+        /// Converts little-endian byte array to int
+        /// </summary>
+        /// <param name="input">le bytes</param>
+        /// <returns>converted number</returns>
         public static int From(params byte[] input) {
             int ret = 0;
 
