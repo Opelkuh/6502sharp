4cfef8fae5104d4a241e3a9d148fc73b129a0f68
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-10-30 21:25:21 +0100

    FlagHelper class

---
+++ 6502sharp/instructions/FlagHelper.cs
@@ -0,0 +1,56 @@
+﻿namespace _6502sharp
+{
+    /// <summary>
+    /// Class that sets flags based on results
+    /// </summary>
+    public static class FlagHelper
+    {
+
+        /// <summary>
+        /// Sets the Carry flag if necessary
+        /// </summary>
+        /// <param name="cpu">target</param>
+        /// <param name="value">value to be checked</param>
+        public static void SetCarry(ICpu cpu, int value)
+        {
+            if (value > 255) cpu.SR.Carry = true;
+            else cpu.SR.Carry = false;
+        }
+
+        /// <summary>
+        /// Sets the Overflow flag based on result and values that made the result
+        /// </summary>
+        /// <param name="cpu">target</param>
+        /// <param name="result">result to be checked</param>
+        /// <param name="in1">original value 1</param>
+        /// <param name="in2">original value 2</param>
+        public static void SetOverflow(ICpu cpu, int result, int in1, int in2)
+        {
+            int calculated = (result ^ in1) & (result ^ in2) & 0x80;
+            if (calculated > 0) cpu.SR.Overflow = true;
+            else cpu.SR.Overflow = false;
+        }
+
+        /// <summary>
+        /// Sets the Zero flag if necessary
+        /// </summary>
+        /// <param name="cpu">target</param>
+        /// <param name="result">value to be checked</param>
+        public static void SetZero(ICpu cpu, int value)
+        {
+            if (value <= 0) cpu.SR.Zero = true;
+            else cpu.SR.Zero = false;
+        }
+
+        /// <summary>
+        /// Sets the Negative flag if necessary
+        /// </summary>
+        /// <param name="cpu">target</param>
+        /// <param name="value">value to be checked</param>
+        public static void SetNegative(ICpu cpu, int value)
+        {
+            if ((value & 0x80) > 0) cpu.SR.Negative = true;
+            else cpu.SR.Negative = false;
+        }
+    }
+}
