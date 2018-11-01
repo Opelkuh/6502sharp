10d1802d823b7da94aa9cc2c568e9247b4653e64
Opelkuh <opelkja15@sps-prosek.cz>
2018-11-01 15:37:23 +0100

    BCDHelper tests

---
+++ 6502sharp.Test/helpers/BCDHelperTest.cs
@@ -0,0 +1,63 @@
+using Xunit;
+using _6502sharp.Helpers;
+
+namespace _6502sharp.Test
+{
+    public class BCDHelperTest : MachineNMOSBase
+    {
+        [Theory]
+        [InlineData(false, 0x01, 0x10)]
+        [InlineData(false, 0xF0, 0xF0)]
+        [InlineData(true, 0x08, 0x08)]
+        [InlineData(true, 0xFF, 0xFF)]
+        public void RetursHalfCarryFlag(bool expected, params int[] vals)
+        {
+            bool ret = BCDHelper.GetHalfCarry(vals);
+
+            Assert.Equal(expected, ret);
+        }
+
+        [Theory]
+        [InlineData(false, 0x0F, 0x01)]
+        [InlineData(false, 0xF0, 0xF0)]
+        [InlineData(true, 0x25, 0x57)]
+        [InlineData(true, 0xFE, 0xFF)]
+        public void RetursHalfBorrowFlag(bool expected, params int[] values)
+        {
+            bool ret = BCDHelper.GetHalfBorrow(values);
+
+            Assert.Equal(expected, ret);
+        }
+
+        [Theory]
+        [InlineData(false, 0x36 , 0x30, 0x18, 0x18)]
+        [InlineData(true,  0x104, 0x9E, 0x58, 0x46)]
+        [InlineData(true,  0x100, 0x9A, 0x50, 0x50)]
+        public void AdjustsResultOfAddition(
+            bool carryFlag,
+            int adjustedResult,
+            int result,
+            params int[] originalValues
+        )
+        {
+            BCDHelper.AdditionAdjust(machine.CPU, ref result, originalValues);
+
+            Assert.Equal(adjustedResult, result);
+            Assert.Equal(carryFlag, machine.CPU.SR.Carry);
+        }
+
+        [Theory]
+        [InlineData(0x28, 0x2E, 0x55, 0x27)]
+        [InlineData(0x68, 0xCE, 0x25, 0x57)]
+        public void AdjustsResultOfSubstraction(
+            int adjustedResult,
+            int result,
+            params int[] originalValues
+        )
+        {
+            BCDHelper.SubstractionAdjust(machine.CPU, ref result, originalValues);
+
+            Assert.Equal(adjustedResult, result);
+        }
+    }
+}
