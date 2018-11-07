272e05876afbd186f029a24d8fbd7e21ed9697e2
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 16:38:03 +0100

    BCS instruction

---
+++ 6502sharp/instructions/BCS.cs
@@ -0,0 +1,21 @@
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class BCS
+    {
+        private ICpu _cpu;
+
+        public BCS(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0xB0, 2)]
+        public void BCS_Relative([Relative] int target)
+        {
+            if(_cpu.SR.Carry == true) {
+                _cpu.PC.Value = (ushort)(target & 0xFFFF);
+            }
+        }
+    }
+}
