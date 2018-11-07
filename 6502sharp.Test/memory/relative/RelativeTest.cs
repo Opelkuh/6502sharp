5ede80171bc7b2059c7a0049293c680e1dcd079d
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 16:32:12 +0100

    RelativeAttribute tests

---
+++ 6502sharp.Test/memory/relative/RelativeTest.cs
@@ -0,0 +1,27 @@
+using Xunit;
+
+namespace _6502sharp.Test.Memory
+{
+    public class RelativeTest : MachineNMOSBase
+    {
+        RelativeAttribute attr;
+
+        public RelativeTest() : base()
+        {
+            attr = new RelativeAttribute();
+        }
+
+        [Theory]
+        [InlineData(0x0010, 0x40, 0x0050)]
+        [InlineData(100, -50, 50)]
+        public void ReturnsAddress(int pc, int offset, int expected)
+        {
+            byte[] input = { (byte)offset };
+            machine.CPU.PC.Value = (ushort)pc;
+
+            int res = attr.Resolve(machine.CPU, ref input);
+
+            Assert.Equal(expected, res);
+        }
+    }
+}
