2809cf4519deee00baed81531f28a74a109cb29e
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 23:04:02 +0100

    BVC instruction

---
+++ 6502sharp/instructions/BVC.cs
@@ -0,0 +1,19 @@
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class BVC
+    {
+        private ICpu _cpu;
+
+        public BVC(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0x50, 2)]
+        public void BVC_Relative([Relative] int target)
+        {
+            if (_cpu.SR.Overflow == false) _cpu.PC.Value = target;
+        }
+    }
+}
