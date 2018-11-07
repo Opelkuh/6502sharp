f48810d369cac6d4eb1bb97d8bbbf184622f906d
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 18:49:01 +0100

    Add CMOS instructions to BIT

--- 6502sharp/instructions/BIT.cs
+++ 6502sharp/instructions/BIT.cs
@@ -12,12 +12,23 @@ namespace _6502sharp.Instructions
             _cpu = cpu;
         }
 
+        [CPUInstruction(0x89, 2, CPUType.CMOS)]
+        public void BIT_Immediate(byte target)
+        {
+            process(target);
+        }
+
         [CPUInstruction(0x24, 3), ZeroPage]
         [CPUInstruction(0x2C, 4), AbsoluteAddress]
+        [CPUInstruction(0x3C, 4, CPUType.CMOS), AbsoluteAddressX]
+        [CPUInstruction(0x34, 4, CPUType.CMOS), ZeroPageX]
         public void BIT_Memory(int address)
         {
-            byte target = _cpu.Memory.Get(address);
+            process(_cpu.Memory.Get(address));
+        }
 
+        private void process(byte target)
+        {
             // set flags
             _cpu.SR.Negative = ((1 << 7) & target) > 0;
             _cpu.SR.Overflow = ((1 << 6) & target) > 0;
