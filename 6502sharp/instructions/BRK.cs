040dc9e9630af4d533048d55f1faa14417781451
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 23:00:47 +0100

    BRK instruction

---
+++ 6502sharp/instructions/BRK.cs
@@ -0,0 +1,36 @@
+using _6502sharp.Helpers;
+
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class BRK
+    {
+        private ICpu _cpu;
+
+        public BRK(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0x00, 7)]
+        public void BRK_Implied()
+        {
+            // save PC + 1
+            byte[] oldPc = LEHelper.To(_cpu.PC.Value + 1, 2); 
+            _cpu.Stack.Push(oldPc);
+
+            // save status reg
+            _cpu.SR.Break = true;
+            _cpu.Stack.Push(_cpu.SR.Value);
+            _cpu.SR.Break = false;
+
+            // set interrupt flag
+            _cpu.SR.Interrupt = true;
+            
+            // set new PC
+            byte[] target = { _cpu.Memory.Get(0xFFFE), _cpu.Memory.Get(0xFFFF) };
+
+            _cpu.PC.Value = LEHelper.From(target);
+        }
+    }
+}
