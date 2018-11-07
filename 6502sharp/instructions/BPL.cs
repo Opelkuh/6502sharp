8437922ff4b9bafe6eba59f39c6320d87e016ef1
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 23:01:20 +0100

    Fix BPL instruction

---
+++ 6502sharp/instructions/BPL.cs
@@ -0,0 +1,19 @@
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class BPL
+    {
+        private ICpu _cpu;
+
+        public BPL(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0x10, 2)]
+        public void BPL_Relative([Relative] int target)
+        {
+            if (_cpu.SR.Negative == false) _cpu.PC.Value = target;
+        }
+    }
+}
