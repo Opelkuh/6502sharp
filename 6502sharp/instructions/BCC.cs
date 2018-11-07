0fbf8be06e122d19d62c764b6bd7871b44177294
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 16:37:44 +0100

    BCC instruction

---
+++ 6502sharp/instructions/BCC.cs
@@ -0,0 +1,21 @@
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class BCC
+    {
+        private ICpu _cpu;
+
+        public BCC(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0x90, 2)]
+        public void BCC_Relative([Relative] int target)
+        {
+            if(_cpu.SR.Carry == false) {
+                _cpu.PC.Value = (ushort)(target & 0xFFFF);
+            }
+        }
+    }
+}
