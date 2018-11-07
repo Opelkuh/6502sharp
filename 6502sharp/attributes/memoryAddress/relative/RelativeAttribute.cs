2296f26cae635edbf569e818de0f7c03f9b52b85
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 16:25:05 +0100

    Add page boundary bug

--- 6502sharp/attributes/memoryAddress/relative/RelativeAttribute.cs
+++ 6502sharp/attributes/memoryAddress/relative/RelativeAttribute.cs
@@ -8,7 +8,11 @@ namespace _6502sharp
         {
             sbyte offset = (sbyte)rawAddress[0];
 
-            return cpu.PC.Value + offset;
+            int address = cpu.PC.Value + offset;
+
+            if (cpu.Type == CPUType.CMOS || (address & 0xFF00) != (cpu.PC.Value & 0xFF00)) cpu.SleepCycles++;
+
+            return address;
         }
     }
 }
