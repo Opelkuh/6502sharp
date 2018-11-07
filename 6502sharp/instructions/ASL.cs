8de23d03ed24485bd4ea7f2191c86ceaca230da4
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-07 16:37:37 +0100

    Fix formatting

--- 6502sharp/instructions/ASL.cs
+++ 6502sharp/instructions/ASL.cs
@@ -3,15 +3,18 @@ using _6502sharp.Helpers;
 namespace _6502sharp.Instructions
 {
     [InjectableInstruction]
-    public class ASL {
+    public class ASL
+    {
         private ICpu _cpu;
 
-        public ASL(ICpu cpu) {
+        public ASL(ICpu cpu)
+        {
             _cpu = cpu;
         }
 
         [CPUInstruction(0x0A, 2)]
-        public void ASL_Accumulator() {
+        public void ASL_Accumulator()
+        {
             _cpu.A.Value = process(_cpu.A.Value);
         }
 
@@ -19,14 +22,16 @@ namespace _6502sharp.Instructions
         [CPUInstruction(0x16, 6), ZeroPageX]
         [CPUInstruction(0x0E, 6), AbsoluteAddress]
         [CPUInstruction(0x1E, 7), AbsoluteAddressX]
-        public void ASL_Memory(int address) {
+        public void ASL_Memory(int address)
+        {
             byte value = _cpu.Memory.Get(address);
             byte shifted = process(value);
 
             _cpu.Memory.Set(address, shifted);
         }
 
-        private byte process(byte value) {
+        private byte process(byte value)
+        {
             int shifted = value << 1;
 
             FlagHelper.SetCarry(_cpu, shifted);
