a0e1cc78bd6c178257ecf8d631c471332c95a4e9
Opelkuh <opelkja15@sps-prosek.cz>
2018-11-08 15:53:52 +0100

    Compare index register instructions

---
+++ 6502sharp/instructions/CPX.cs
@@ -0,0 +1,33 @@
+using _6502sharp.Helpers;
+
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class CPX
+    {
+        private ICpu _cpu;
+
+        public CPX(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0xE0, 2)]
+        public void CPX_Immediate(byte param)
+        {
+            process(param);
+        }
+
+        [CPUInstruction(0xE4, 3), ZeroPage]
+        [CPUInstruction(0xEC, 4), AbsoluteAddress]
+        public void CPX_Memory(int address)
+        {
+            process(_cpu.Memory.Get(address));
+        }
+
+        private void process(byte value)
+        {
+            CompareHelper.RegisterAndValue(_cpu, _cpu.X.Value, value);
+        }
+    }
+}
---
+++ 6502sharp/instructions/CPY.cs
@@ -0,0 +1,33 @@
+using _6502sharp.Helpers;
+
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class CPY
+    {
+        private ICpu _cpu;
+
+        public CPY(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0xC0, 2)]
+        public void CPY_Immediate(byte param)
+        {
+            process(param);
+        }
+
+        [CPUInstruction(0xC4, 3), ZeroPage]
+        [CPUInstruction(0xCC, 4), AbsoluteAddress]
+        public void CPY_Memory(int address)
+        {
+            process(_cpu.Memory.Get(address));
+        }
+
+        private void process(byte value)
+        {
+            CompareHelper.RegisterAndValue(_cpu, _cpu.Y.Value, value);
+        }
+    }
+}
