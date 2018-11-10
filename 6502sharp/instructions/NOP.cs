a85b7aa22c2e750487b2272c7a12fc0c0056e0e6
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-10 23:02:38 +0100

    NOP instruction

---
+++ 6502sharp/instructions/NOP.cs
@@ -0,0 +1,19 @@
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class NOP
+    {
+        private ICpu _cpu;
+
+        public NOP(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0xEA, 2)]
+        public void NOP_Implied()
+        {
+            // do nothing :)
+        }
+    }
+}
