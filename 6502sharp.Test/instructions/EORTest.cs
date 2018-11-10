68d5c4a0c5343e77f2f38e15a46f4ccb22a4fd62
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-10 16:14:57 +0100

    EOR instruction tests

---
+++ 6502sharp.Test/instructions/EORTest.cs
@@ -0,0 +1,52 @@
+using Xunit;
+using _6502sharp.Instructions;
+
+namespace _6502sharp.Test.Instructions
+{
+    public class EORTest : MachineCMOSBase
+    {
+        EOR _dec;
+
+        public EORTest()
+        {
+            _dec = new EOR(machine.CPU);
+        }
+
+        [Theory]
+        [InlineData(0x81, 0x7E, 0xFF)]
+        [InlineData(0xFF, 0xFF, 0x00)]
+        [InlineData(0x60, 0xFF, 0x9F)]
+        public void XORsImmediate(
+            byte accu,
+            byte value,
+            byte expected
+        )
+        {
+            machine.CPU.A.Value = accu;
+
+            _dec.EOR_Immediate(value);
+
+            Assert.Equal(expected, machine.CPU.A.Value);
+        }
+
+        [Theory]
+        [InlineData(0x81, 0x7E, 0xFF)]
+        [InlineData(0xFF, 0xFF, 0x00)]
+        [InlineData(0x60, 0xFF, 0x9F)]
+        public void XORsMemory(
+            byte accu,
+            byte value,
+            byte expected
+        )
+        {
+            int memAddress = 0xABBA;
+
+            machine.CPU.A.Value = accu;
+            machine.Memory.Set(memAddress, value);
+
+            _dec.EOR_Memory(memAddress);
+
+            Assert.Equal(expected, machine.CPU.A.Value);
+        }
+    }
+}
