0e3fab143b78db718459ae638363439adc688589
Opelka Jakub <opelkja15@sps-prosek.cz>
2018-11-10 17:33:42 +0100

    INC instruction

---
+++ 6502sharp/instructions/INC.cs
@@ -0,0 +1,40 @@
+using _6502sharp.Helpers;
+
+namespace _6502sharp.Instructions
+{
+    [InjectableInstruction]
+    public class INC
+    {
+        private ICpu _cpu;
+
+        public INC(ICpu cpu)
+        {
+            _cpu = cpu;
+        }
+
+        [CPUInstruction(0x1A, 2, CPUType.CMOS)]
+        public void INC_Accumulator()
+        {
+            _cpu.A.Value++;
+
+            FlagHelper.SetNegative(_cpu, _cpu.A.Value);
+            FlagHelper.SetZero(_cpu, _cpu.A.Value);
+        }
+
+        [CPUInstruction(0xE6, 5), ZeroPage]
+        [CPUInstruction(0xF6, 6), ZeroPageX]
+        [CPUInstruction(0xEE, 6), AbsoluteAddress]
+        [CPUInstruction(0xFE, 7), AbsoluteAddressX]
+        public void INC_Memory(int address)
+        {
+            byte val = _cpu.Memory.Get(address);
+
+            val++;
+
+            FlagHelper.SetNegative(_cpu, val);
+            FlagHelper.SetZero(_cpu, val);
+
+            _cpu.Memory.Set(address, val);
+        }
+    }
+}
