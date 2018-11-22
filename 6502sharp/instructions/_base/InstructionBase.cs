b72e87eb47acd9ed2c4515ae2b6bee839e873cda
Opelkuh <opelkja15@sps-prosek.cz>
2018-11-22 23:29:04 +0100

    Add comments

--- 6502sharp/instructions/_base/InstructionBase.cs
+++ 6502sharp/instructions/_base/InstructionBase.cs
@@ -2,9 +2,21 @@
 
 namespace _6502sharp.Instructions {
     public abstract class InstructionBase {
+        /// <summary>
+        /// Injected CPU
+        /// </summary>
         protected ICpu cpu;
+        /// <summary>
+        /// CompareHelper bound to the injected CPU
+        /// </summary>
         protected CompareHelper compare;
+        /// <summary>
+        /// FlagHelper bound to the injected CPU
+        /// </summary>
         protected FlagHelper flags;
+        /// <summary>
+        /// BCDHelper bound to the injected CPU
+        /// </summary>
         protected BCDHelper bcd;
 
         public InstructionBase(ICpu cpu) {
