79e499fb2b20669e76b72896ebafac57748cc64e
Opelkuh <opelkja15@sps-prosek.cz>
2018-12-06 15:47:58 +0100

    Main loop cleanup

--- 6502sharp.Debug/Program.cs
+++ 6502sharp.Debug/Program.cs
@@ -8,34 +8,35 @@ namespace _6502sharp.Debug
     {
         static void Main(string[] args)
         {
-            Start();
+            Start("snake.bin", true);
         }
 
-        private static void Start()
+        private static void Start(string rom, bool loop = false)
         {
             ConsoleRenderer renderer = new ConsoleRenderer(32, 32);
             GameMemory mem = new GameMemory(renderer);
 
             IMachine mach = new NMOSMachine(mem);
 
-            // LoadRom(mach, "./roms/compo-1st.bin", 0x0600);
-            LoadRom(mach, "./roms/snake.bin", 0x0600);
+            LoadRom(mach, $"./roms/{rom}", 0x0600);
 
-            DateTime nextTick = DateTime.Now;
             while (true)
-            {
-                if (DateTime.Now < nextTick) continue;
-
+            {   
+                // input poll
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo pressed = Console.ReadKey(true);
+
+                    // exit if escape
+                    if(pressed.Key == ConsoleKey.Escape) break;
+
                     mach.Memory.Set(0xFF, (byte)pressed.KeyChar);
                 }
 
                 mach.CPU.Tick();
 
-                if (mach.CPU.PC.Value == 0) {
-                    Start();
+                if (loop && mach.CPU.PC.Value == 0) {
+                    Start(rom, loop);
                     break;
                 }
             }
