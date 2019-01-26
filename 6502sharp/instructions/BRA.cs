a7cbadb91ce6b0ddf1dfd7f438a8719e4399f875
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-01-26 19:34:11 +0100

    Add BRA instruction for CMOS

---
+++ 6502sharp/instructions/BRA.cs
@@ -0,0 +1,16 @@
+namespace _6502sharp.Instructions
+{
+    [DefaultInstruction]
+    public class BRA : InstructionBase
+    {
+        public BRA(ICpu cpu) : base(cpu)
+        {
+        }
+
+        [CPUInstruction(0x80, 2, CPUType.CMOS)]
+        public void BRA_Relative([Relative] int target)
+        {
+            cpu.PC.Value = (ushort)target;
+        }
+    }
+}
