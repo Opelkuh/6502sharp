e4a3612a8fb94545e2e235253a5de2544fac9b4b
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-01-26 18:43:03 +0100

    Add menu to demo

---
+++ 6502sharp.Debug/Menu.cs
@@ -0,0 +1,77 @@
+using System;
+
+namespace _6502sharp.Debug
+{
+    class Menu
+    {
+        private MenuItem[] items;
+        private int selected = 0;
+
+        public Menu(params MenuItem[] items)
+        {
+            this.items = items;
+        }
+
+        public MenuItem Start()
+        {
+            bool loop = true;
+            while (loop)
+            {
+                Console.Clear();
+                Console.WriteLine();
+                WriteCenter("SELECT ROM");
+                Console.WriteLine();
+                PrintOptions();
+
+                var pressed = Console.ReadKey(true);
+                switch (pressed.Key)
+                {
+                    case ConsoleKey.W:
+                    case ConsoleKey.UpArrow:
+                        selected--;
+                        if (selected < 0) selected = items.Length - 1;
+                        break;
+                    case ConsoleKey.S:
+                    case ConsoleKey.DownArrow:
+                        selected++;
+                        selected %= items.Length;
+                        break;
+                    case ConsoleKey.Enter:
+                    case ConsoleKey.Spacebar:
+                        loop = false;
+                        break;
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+            return items[selected];
+        }
+
+        private void PrintOptions()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                var item = items[i];
+
+                if (i == selected)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    WriteCenter($"> {item.displayValue} <");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    WriteCenter(item.displayValue);
+                }
+            }
+        }
+
+        private void WriteCenter(string s)
+        {
+            Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
+            Console.WriteLine(s);
+        }
+    }
+}
--- 6502sharp.Debug/Program.cs
+++ 6502sharp.Debug/Program.cs
@@ -4,11 +4,27 @@ namespace _6502sharp.Debug
 {
     class Program
     {
+        private static MenuItem[] roms =
+        {
+            new MenuItem("Snake", "snake.bin"),
+            new MenuItem("Calculator", "calculator.bin"),
+            new MenuItem("Alive", "alive.bin"),
+            new MenuItem("Brick", "brick.bin"),
+            new MenuItem("Colors", "colors.bin"),
+            new MenuItem("Compo-1st", "compo-1st.bin")
+        };
+
         static void Main(string[] args)
         {
             GameConsole cons = new GameConsole();
+            Menu menu = new Menu(roms);
 
-            cons.StartGame("snake.bin", true);
+            while (true)
+            {
+                var selected = menu.Start();
+
+                cons.StartGame(selected.value, true);
+            }
         }
     }
 }
