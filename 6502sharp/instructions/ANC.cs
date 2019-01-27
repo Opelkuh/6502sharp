17678c4490cf478f8690100ca8368d1d92d8f555
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-01-27 16:16:18 +0100

    Add ANC undocumented instruction

---
+++ 6502sharp/instructions/ANC.cs
@@ -0,0 +1,22 @@
+using _6502sharp.Helpers;
+
+namespace _6502sharp.Instructions
+{
+    [DefaultInstruction]
+    public class ANC : InstructionBase
+    {
+        public ANC(ICpu cpu) : base(cpu)
+        {
+        }
+
+        [CPUInstruction(0x0B, 2)]
+        [CPUInstruction(0x2B, 2)]
+        public void ANC_Immediate(byte value)
+        {
+            int res = cpu.A.Value & value;
+
+            flags.SetNegativeAndZero(res);
+            if ((res & 0x80) > 0) cpu.SR.Carry = true;
+        }
+    }
+}
