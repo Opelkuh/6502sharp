3ea753bcae8a168d3737fc362cd266d95915c31c
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-02-05 21:22:53 +0100

    Add SBX undocumented instruction

---
+++ 6502sharp/instructions/SBX.cs
@@ -0,0 +1,26 @@
+using _6502sharp.Helpers;
+
+namespace _6502sharp.Instructions
+{
+    [DefaultInstruction]
+    public class SBX : InstructionBase
+    {
+        public SBX(ICpu cpu) : base(cpu)
+        {
+        }
+
+        [CPUInstruction(0xCB, 2)]
+        public void SBX_Immediate(byte value)
+        {
+            int and = cpu.X.Value & cpu.A.Value;
+            int sub = and - value;
+            byte res = (byte)sub;
+
+            cpu.X.Value = res;
+
+            // set flags
+            cpu.SR.Carry = sub >= 0;
+            flags.SetNegativeAndZero(res);
+        }
+    }
+}
