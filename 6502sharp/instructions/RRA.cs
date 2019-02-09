8a918ea0b3a7a468ef9b45b642c42c5772db622b
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-02-09 15:58:06 +0100

    Add RLA and RRA undocumented instruction

---
+++ 6502sharp/instructions/RRA.cs
@@ -0,0 +1,35 @@
+using _6502sharp.Helpers;
+
+namespace _6502sharp.Instructions
+{
+    [DefaultInstruction]
+    public class RRA : InstructionBase
+    {
+        private ROR ror;
+        private ADC adc;
+
+        public RRA(ICpu cpu) : base(cpu)
+        {
+            this.ror = new ROR(cpu);
+            this.adc = new ADC(cpu);
+        }
+
+        [CPUInstruction(0x67, 5), ZeroPage]
+        [CPUInstruction(0x77, 6), ZeroPageX]
+        [CPUInstruction(0x6F, 6), Absolute]
+        [CPUInstruction(0x7F, 7), AbsoluteX]
+        [CPUInstruction(0x7B, 7), AbsoluteY]
+        [CPUInstruction(0x63, 8), IndirectX]
+        [CPUInstruction(0x73, 8), IndirectY]
+        public void RRA_Memory(int address)
+        {
+            ror.ROR_Memory(address);
+            bool carry = cpu.SR.Carry;
+
+            adc.ADC_Memory(address);
+
+            // set carry from ROR
+            cpu.SR.Carry = carry;
+        }
+    }
+}
