443605dbaf1fed376d68919f038386e48c857b2d
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-02-13 18:21:45 +0100

    Add NES example project

---
+++ Examples/NES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
