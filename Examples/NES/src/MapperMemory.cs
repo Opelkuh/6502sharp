66d6048fb45ae1ee5e9a45b8fe336b154a0a3511
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-02-14 22:42:42 +0100

    NES - Mirror memory

--- Examples/NES/src/MapperMemory.cs
+++ Examples/NES/src/MapperMemory.cs
@@ -4,6 +4,33 @@ namespace NES
 {
     class MapperMemory : Memory
     {
-        public MapperMemory() : base(65536) { }
+        Mirror[] mirrors = new Mirror[] {
+            new Mirror(0x0000, 0x07FF, 0x0800, 0x1FFF),
+            new Mirror(0x2000, 0x2007, 0x2008, 0x3FFF)
+    };
+
+        public MapperMemory() : base(65536)
+        {
+        }
+
+        public override byte Get(int location)
+        {
+            CheckMirror(ref location);
+            return base.Get(location);
+        }
+
+        public override void Set(int location, byte value)
+        {
+            CheckMirror(ref location);
+            base.Set(location, value);
+        }
+
+        private void CheckMirror(ref int loc)
+        {
+            foreach (var mir in mirrors)
+            {
+                if (mir.Fits(loc)) loc = mir.Wrap(loc);
+            }
+        }
     }
 }
