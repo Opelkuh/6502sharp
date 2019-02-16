924811431a881435f4571ca560f1ae39bb1a4951
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-02-16 19:45:35 +0100

    Add IMapper

--- Examples/NES/src/MapperMemory.cs
+++ Examples/NES/src/MapperMemory.cs
@@ -4,10 +4,22 @@ namespace NES
 {
     class MapperMemory : Memory
     {
-        Mirror[] mirrors = new Mirror[] {
+        public IMapper Mapper
+        {
+            get => _mapper;
+            set
+            {
+                if (_mapper != null) _mapper.Dispose();
+
+                _mapper = value;
+            }
+        }
+
+        private Mirror[] mirrors = new Mirror[] {
             new Mirror(0x0000, 0x07FF, 0x0800, 0x1FFF),
             new Mirror(0x2000, 0x2007, 0x2008, 0x3FFF)
-    };
+        };
+        private IMapper _mapper;
 
         public MapperMemory() : base(65536)
         {
@@ -15,12 +27,23 @@ namespace NES
 
         public override byte Get(int location)
         {
+            if (_mapper != null)
+            {
+                byte? res = _mapper.Get(location);
+
+                // return if handled by mapper
+                if (res.HasValue) return res.Value;
+            }
+
             CheckMirror(ref location);
             return base.Get(location);
         }
 
         public override void Set(int location, byte value)
         {
+            // return if handled by mapper
+            if (_mapper != null && _mapper.Set(location, value)) return;
+
             CheckMirror(ref location);
             base.Set(location, value);
         }
