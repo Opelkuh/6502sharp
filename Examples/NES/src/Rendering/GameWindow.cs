cfd46c6fe916771a27c3fa065ff96993ff23ba4f
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-02-20 17:18:26 +0100

    Move GameWindow to NES.Rendering namespace

--- Examples/NES/src/Rendering/GameWindow.cs
+++ Examples/NES/src/Rendering/GameWindow.cs
@@ -3,33 +3,36 @@ using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
-sealed class GameWindow : OpenTK.GameWindow
+namespace NES.Rendering
 {
-    public const string WINDOW_TITLE = "6502sharp - NES Emulator";
-
-    public GameWindow()
-        : base(256, 240, GraphicsMode.Default, WINDOW_TITLE)
+    class GameWindow : OpenTK.GameWindow
     {
-        Console.WriteLine("Renderer: " + GL.GetString(StringName.Renderer));
-        Console.WriteLine("OpenGL Version: " + GL.GetString(StringName.Version));
-    }
+        public const string WINDOW_TITLE = "6502sharp - NES Emulator";
 
-    protected override void OnResize(EventArgs e)
-    {
-        GL.Viewport(0, 0, this.Width, this.Height);
-    }
+        public GameWindow()
+            : base(256, 240, GraphicsMode.Default, WINDOW_TITLE)
+        {
+            Console.WriteLine("Renderer: " + GL.GetString(StringName.Renderer));
+            Console.WriteLine("OpenGL Version: " + GL.GetString(StringName.Version));
+        }
 
-    protected override void OnRenderFrame(FrameEventArgs e)
-    {
-        GL.ClearColor(Color4.Black);
-        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, this.Width, this.Height);
+        }
 
-        GL.Begin(BeginMode.Triangles);
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.ClearColor(Color4.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Begin(BeginMode.Triangles);
             GL.Color3(1.0f, 0.0f, 0.0f); GL.Vertex2(0.0f, 1.0f);
             GL.Color3(0.0f, 1.0f, 0.0f); GL.Vertex2(0.87f, -0.5f);
             GL.Color3(0.0f, 0.0f, 1.0f); GL.Vertex2(-0.87f, -0.5f);
-        GL.End();
+            GL.End();
 
-        this.SwapBuffers();
+            this.SwapBuffers();
+        }
     }
 }
