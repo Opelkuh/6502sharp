dc12f4e31baf1902587d8dd4a1ff02ab7fd037a1
Opelka Jakub <opelkja15@sps-prosek.cz>
2019-02-21 12:53:18 +0100

    Add OAM DMA

--- Examples/NES/src/PPU/PPU.OAM.cs
+++ Examples/NES/src/PPU/PPU.OAM.cs
@@ -4,7 +4,21 @@ namespace NES.PPU
     {
         private void startDMA(byte hiByte)
         {
-            // TODO: Direct Memory Access OAM transfer
+            // add sleep cycles to CPU
+            mach.CPU.SleepCycles += 513;
+
+            // add extra cycle if FinishedCycles is odd after transfer
+            int total = mach.CPU.FinishedCycles + mach.CPU.SleepCycles;
+            if ((total & 1) == 0) mach.CPU.SleepCycles++;
+
+            // DMA transfer
+            int start = hiByte << 8;
+            int end = start | 0x00FF;
+            for (int i = start; i <= end; i++)
+            {
+                byte val = mach.Memory.Get(i);
+                OAM.Set(OAMAddress++, val);
+            }
         }
     }
 }
